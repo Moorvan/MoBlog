a654721f0a2bdbdcd0d0b5128abb737d3de43787
mufanx <35735090+mufanx@users.noreply.github.com>
2022-05-24 13:30:51 +0800

    ssl

--- server/server.go
+++ server/server.go
@@ -10,5 +10,6 @@ func main() {
 	app := fiber.New()
 	app.Use(logger.New())
 	app.Static("/", "../hugoWeb/public")
-	log.Fatalln(app.Listen(":80"))
+	log.Fatalln(app.ListenTLS(":80", "/etc/letsencrypt/live/morvan.dev/fullchain.pem", "/etc/letsencrypt/live/morvan.dev/privkey.pem"))
+	//log.Fatalln(app.Listen(":80"))
 }
